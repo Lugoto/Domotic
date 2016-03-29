94ce9aad6f9f75cc6046945293dd0be3278490f7
Lucas Pozzer <Lucas Pozzer>
2016-03-29 15:09:15 +0100

    Premier commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="description" content="Made with WOW Slider - Create beautiful, responsive image sliders in a few clicks. Awesome skins and animations. Slider" />
+    <link rel="stylesheet" type="text/css" href="engine1/style.css" />
+    <script type="text/javascript" src="engine1/jquery.js"></script>
+    <title></title>
+  </head>
+  <body>
+    <div class="menu">
+      <div class="titre">
+        Domotic
+      </div>
+      <nav id="nav-1">
+        <a class="link-1" href="#">Chambre</a>
+        <a class="link-1" href="#">Cuisine</a>
+        <a class="link-1" href="#">Salle de bain</a>
+        <a class="link-1" href="#">Salon</a>
+      </nav>
+    </div>
+    <div id="wowslider-container1">
+  	<div class="ws_images"><ul>
+  		<li><img src="data1/images/domotiqueinfolinkioschema.png" alt="http://wowslider.com/" title="domotique-info-linkio-schema" id="wows1_0"/></li>
+  		<li><img src="data1/images/domotiquemaison1.jpg" alt="domotique-maison-1" title="domotique-maison-1" id="wows1_1"/></li>
+  	</ul></div>
+  	<div class="ws_bullets"><div>
+  		<a href="#" title="domotique-info-linkio-schema"><span><img src="data1/tooltips/domotiqueinfolinkioschema.png" alt="domotique-info-linkio-schema"/>1</span></a>
+  		<a href="#" title="domotique-maison-1"><span><img src="data1/tooltips/domotiquemaison1.jpg" alt="domotique-maison-1"/>2</span></a>
+  	</div></div><div class="ws_script" style="position:absolute;left:-99%"><a href="http://wowslider.com/vi">slider</a> by WOWSlider.com v8.7</div>
+  	<div class="ws_shadow"></div>
+  	</div>
+  	<script type="text/javascript" src="engine1/wowslider.js"></script>
+  	<script type="text/javascript" src="engine1/script.js"></script>
+
+  </body>
+</html>
